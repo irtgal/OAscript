95cfebbd11b07063fe733bb06ea9361bf92ca315
irtgal <gal.irt.01@gmail.com>
2021-04-26 11:23:17 +0200

    version 1.0

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import re
+from scripts import *
+from Comment import *
+from ApiResource import *
+
+
+#directories 
+baseDir = "C:/Users/Student/Desktop/result/backend"
+
+controllersDir = baseDir + "/app/Http/Controllers"
+routePath = baseDir + "/routes/api.php"
+
+
+routeLines = setApiFile(routePath)
+
+
+normalPattern = re.compile(r"^Route::(post|get|patch|delete|any)\(\'(.*)\'\,[\s]*\'(.*)@(.*)\'\)\;")
+resourcePattern = re.compile(r"^Route::apiResource\(\'(.*)\'\,[\s]*\'(.*)\'\)\;")
+routesCount = otherCount =  0
+startIndex = int(input("Select starting index: "))
+for line in routeLines:
+    normalMatch = normalPattern.search(line)
+    resourceMatch = resourcePattern.search(line)
+    if normalMatch:
+        if routesCount >= startIndex:
+            try:
+                methodType, path, fileName, funcName = normalMatch.groups()
+                c = Comment(methodType, path, fileName, funcName)
+                webbrowser.open(f'{c.controllersDir}/{c.fileName}.php')
+                formComment(c, routesCount)
+                c.writeComment()
+            except Exception as error:
+                print(f"ERROR: {error} - {line}")
+        routesCount += 1
+
+    elif resourceMatch:
+        if routesCount >= startIndex:
+            try:
+                path, fileName = resourceMatch.groups()
+                apiresource = ApiResource(path, fileName)
+                for funcName, methodType in apiresource.commentsDict.items():
+                    c = Comment(methodType, path, fileName, funcName)
+                    webbrowser.open(f'{c.controllersDir}/{c.fileName}.php')
+                    formComment(c, routesCount)
+                    c.writeComment()
+            except Exception as error:
+                 print(f"ERROR: {error} - {line}")
+        routesCount += 1
+    else:
+        otherCount += 1
+
+print(f"Routes: {routesCount}, other: {otherCount}")
